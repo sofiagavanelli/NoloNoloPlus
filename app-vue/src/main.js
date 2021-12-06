b34be75c77049970e0008e8ef0ba59d2a55ef69c
Sofia Gavanelli <sofia.gavanelli@studio.unibo.it>
2021-12-06 11:12:16 +0100

    continua a non funzionare nulla

--- app-vue/src/main.js
+++ app-vue/src/main.js
@@ -3,6 +3,12 @@ console.log("sono dentro main");
 import Vue from 'vue'
 import App from '../src/App.vue'
 
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
 
 new Vue({
