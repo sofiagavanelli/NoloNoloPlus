31540d4c5a4aefaa351313a90cd55c26d471944d
Sofia Gavanelli <sofia.gavanelli@studio.unibo.it>
2021-12-06 14:49:17 +0100

    tolta app vue

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/public", express.static(path.resolve(__dirname, 'public')));
 require("./db");
 
 
-/*Home page VERSIONE NO VUE */
+/*Home page VERSIONE NO VUE *
 app.get("/",function (req, res) {
 
     res.sendFile(path.join(__dirname + "/app-vue/public/index.html"));
@@ -46,7 +46,7 @@ app.get("/",function (req, res) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
-    }); */
+    }); *
     
 });
 
@@ -72,6 +72,23 @@ app.get("/",function (req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
     
 });*/
+
+//SI APRE DIRETTAMENTE IL WORKER
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/reserved.html"));
+  
+      /*fs.readFile("index.html", function (err, data) {
+          if (err) {
+              res.writeHead(404, { 'Content-Type': 'text/html' });
+              return res.end("<h1>404 Not Found</h1>");
+          }
+  
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write(data);
+          return res.end();
+      });*/
+      
+  });
 
 //Ambiente worker
 app.get('/worker-module', function (req, res) {
