55a433f9c60971e2dfa9e09104b53bb9bab7d3a7
fedepales <72525421+fedepales@users.noreply.github.com>
2022-01-03 11:08:26 +0100

    added css

--- manager-react/src/pages/Dipendenti.js
+++ manager-react/src/pages/Dipendenti.js
@@ -1,7 +1,8 @@
 import {ChartProva} from '../components/provaChart';
+import "../App.css";
 function Dipendenti() {
   return (
-    <div>
+    <div id="dipendenti">
         <h1>Dipendenti</h1>
         <ChartProva></ChartProva>
     </div>
