f3d04d915a4cf23915707c755f9ac262ae53decb
fedepales <72525421+fedepales@users.noreply.github.com>
2022-01-14 11:58:32 +0100

    added card and bar chart customers

--- manager-react/src/pages/Clienti.js
+++ manager-react/src/pages/Clienti.js
@@ -1,11 +1,17 @@
 import "../App.css";
+import {BarChartClient} from '../components/barChartClient';
+import {CardClienti} from '../components/cardClienti';
 
 function Clienti() {
     return (
       <div id="clienti">
-        <h1>Clienti</h1>
+        <h1>Customers Statistics</h1>
+        <BarChartClient></BarChartClient>
+        <h1>Customers </h1>
+        <CardClienti></CardClienti>
       </div>
       );
 }
+
 
 export default Clienti;
